3cd94a911194557c77da97430112e4aba9741a5e
Ken Campbell <8246018+kenatcampbellmusclelab@users.noreply.github.com>
2020-11-11 16:55:02 -0500

    Adding in demo_fit_two_by_two_curves_tension_pCa

--- code/fit/extract_p_data_from_opt_structure.m
+++ code/fit/extract_p_data_from_opt_structure.m
@@ -23,7 +23,7 @@ else
     end
 end
 
-% Now pull out par_values and par_labels
+% Now pull out par_values and par_labels for the defined parameters
 for i = 1 : numel(opt_structure.parameter)
     par_values(i) = return_parameter_value( ...
         opt_structure.parameter{i}, p_vector(i));
@@ -31,14 +31,23 @@ for i = 1 : numel(opt_structure.parameter)
 end
 
 % Now search through constraints
+par_counter = numel(par_labels);
 if (isfield(opt_structure, 'constraint'))
     for i = 1 : numel(opt_structure.constraint)
         c = opt_structure.constraint{i};
         if (isfield(c, 'parameter_multiplier'))
             for j = 1 : numel(c.parameter_multiplier)
-                par_values(numel(p_vector)) = return_parameter_value( ...
-                    c.parameter_multiplier{j}, p_vector(end));
-                par_labels{numel(p_vector)} = ...
+                par_counter = par_counter + 1;
+%                 
+%                 cc = c.parameter_multiplier{j}
+%                 pp = p_vector(par_counter)
+%                 yy = return_parameter_value( ...
+%                     c.parameter_multiplier{j}, p_vector(par_counter))
+% 
+%                 
+                par_values(par_counter) = return_parameter_value( ...
+                    c.parameter_multiplier{j}, p_vector(par_counter));
+                par_labels{par_counter} = ...
                     sprintf('mult_%s',c.parameter_multiplier{j}.name);
             end
         end
